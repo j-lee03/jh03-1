24cbe8361f90d53fcc686b98bbe6106203348898
j.lee <sdf090400@gmail.com>
2025-10-10 18:00:29 +0900

    1

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
-import pathlib
-
-import app
-from app.routers import home, about, contact, projects
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.routers import home, about, contact, projects
 
-# ...
+app = FastAPI()
 
+# Static files (정적 파일) 연결
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Routers (각 페이지) 연결
 app.include_router(home.router)
 app.include_router(about.router)
-app.include_router(projects.router)
 app.include_router(contact.router)
+app.include_router(projects.router)
